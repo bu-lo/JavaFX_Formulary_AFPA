7bbb4fc2903cef38e0ceffb9e24b839821f2c8d5
bu-lo <161450788+bu-lo@users.noreply.github.com>
2024-07-11 16:20:26 +0200

    End TD graphic components and 3 buttons

--- src/main/java/fr/afpa/App.java
+++ src/main/java/fr/afpa/App.java
@@ -6,6 +6,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -21,24 +24,63 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = SystemInfo.javaVersion();
-        String javafxVersion = SystemInfo.javafxVersion();
+        stage.setMaxHeight(144);
+        stage.setMaxWidth(400);
 
         //INSTANCIATION
-        Label label = new Label("Hello");
-        Label label2 = new Label("World !");
-        Label label3 = new Label("\n");
-        Button button1 = new Button("Quit");
+        Label label = new Label("User Input :  ");
+        Label label2 = new Label("Copy of the User Input : ");
 
-        VBox vbox1 = new VBox(label, label2, label3, button1);
+        TextField textField1 = new TextField("Enter Text");
+        TextField textField2 = new TextField("Enter Text");
+        textField2.setEditable(false);
        
+        Button buttonCopy = new Button("Copy");
+        Button buttonErase = new Button("Erase");
+        Button buttonQuit = new Button("Quit");
+
+        VBox vbox1 = new VBox(buttonCopy,buttonErase,buttonQuit);
+        vbox1.setSpacing(10); //spacing in bbox
+        vbox1.setAlignment(Pos.CENTER);
+
+        GridPane gridPane = new GridPane(); // GRIDPANE
+        gridPane.add(label, 0,0);
+        gridPane.add(label2, 0,1);
+        gridPane.add(textField1,1,0);
+        gridPane.add(textField2,1,1);
+        gridPane.add(vbox1,2,0,1,2);
+
+        //gridPane.setGridLinesVisible(true); -> LINES
+       
+        RowConstraints row1 = new RowConstraints();
+        row1.setPercentHeight(50);
+        RowConstraints row2 = new RowConstraints();
+        row2.setPercentHeight(50);
+
+        gridPane.getRowConstraints().addAll(row1,row2);
+
        //SEETINGS
-        vbox1.setAlignment(Pos.CENTER); //.SETALIGNMENT
+       gridPane.setAlignment(Pos.CENTER); //.SETALIGNMENT
+       gridPane.setHgap(20);
+       gridPane.setVgap(10);
 
         //"ASSEMBLAGE"
-        Scene scene = new Scene(vbox1, 640, 480);
+        Scene scene = new Scene(gridPane, 640, 480);
        
-        button1.setOnAction(e ->Platform.exit());  //.EXIT
+        buttonCopy.setOnAction(e -> {
+        //String text = textField1.getText(); //.GETTEXT
+        //textField2.setText(text);  //.SETTEXT
+        textField2.setText(textField1.getText());
+        });
+       
+
+        buttonErase.setOnAction(e -> {
+        textField1.setText("Enter Text");
+        textField2.setText(textField1.getText());
+        });
+
+
+        buttonQuit.setOnAction(e ->Platform.exit());  //.EXIT
         stage.setScene(scene);
         stage.show();
 
